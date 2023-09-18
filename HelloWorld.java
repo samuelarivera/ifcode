c32d932cb263591170c4a9d5fefaec65f6833d7f
Samuel Rivera <samuel.rivera@mrjaffesclass.com>
2023-09-18 11:31:05 -0700

    if coded based on intructions

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,9 +2,30 @@ public class HelloWorld
 {
     public static void main(String[] args) 
     {
-            int i;   
-            float f = 2.5f;
-            boolean flag = true;
+        int a = 5;
+        int b = 2;
+        int c = 1;
+        int d = -8;
+        if (a == 5) {
+            System.out.println("SUCCESS");
+        } else {
+            System.out.println("WhoopsieDaisy");
+        }
+        if (b == 2) {
+            System.out.println("Success");
+        } else {
+            System.out.println("WhoopsieDaisy");
+        }
+        if (c != -1) {
+            System.out.println("SUCCESS");
+        } else {
+            System.out.println("WhoopsieDaisy");
+        }
+        if (d == -8 & a == 4) {
+            System.out.println("SUCCESS");
+        } else {
+            System.out.println("WhoopsieDaisy");
+        }
+        }
     }
-}
 
